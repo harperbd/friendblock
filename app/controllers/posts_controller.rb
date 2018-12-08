2e8ed34fb36c1843f8accf244dbabc94f8a6f63c
Adriana Vars <asvutah@gmail.com>
2018-12-08 14:12:59 -0700

    more stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,59 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
+    @posts = Post.all
   end
 
-  def show
-  end
+#   def update
+#     if @post.update(post_params)
+#       redirect_to posts_path
+#     else
+#       render :edit
+#     end
+#   end 
 
-  def edit
-  end
+#   def new
+#     # creates a sub in memory 
+#     @post = Post.new 
+#     # renders the sub form 
+#     render partial: "form"
+#   end
+
+  
+#     def create
+#       @post = Post.create(post_params)
+  
+#       if @post.save 
+#         redirect_to root_path
+#       else
+#         render :new
+#       end 
+  
+#     end 
+
+
+#   def show
+
+#   end
+
+#   def edit
+#   end
+
+#   def destroy
+#     @post.destroy
+#     redirect_to posts_path
+#   end 
+
+
+# private
+
+# def post_params
+#   params.require(:post).permit(:body, :user_id)
+# end
+
+# def set_account
+#   @post = current_user.posts.find(params[:id])
+# end
+
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
-  def index
-  end
+    def index
+      @users = current_user.users
+    end
 
   def show
   end
 
+  def new
+    @user = User.new
+  end
+
   def edit
+  end
+
+  def create
+    @user = current_user.users.new(user_params)
+    if @user.save
+      flash[:success] = "Account Created"
+      redirect_to users_path
+    else
+      flash[:error] = "error #{@user.errors.full_messages.join("\n")}"
+      render :new
+    end
   end
 end
