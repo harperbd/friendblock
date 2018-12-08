c49592994788fb54df3bda0474cd203872d23383
Brad.Harper <Brad.Harper@consensuscorp.com>
2018-12-08 13:47:17 -0700

    adding seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+2.times do |i|
+  user = User.new(email: "#{i}_email@gmail.com", first_name: "#{i}_first", last_name: "#{i}_last")
+  user.save!(validate: false)
+
+  1.times do |j|
+    Post.create(body: "#{i}_#{j} This is a post", user_id: user.id)
+  end
+
+end
